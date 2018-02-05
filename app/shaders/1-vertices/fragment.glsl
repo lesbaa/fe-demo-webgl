0d3de7ffb6f731d107d249d5e0fac631e1267338
Les Moffat <les@lesmoffat.co.uk>
2018-02-05 22:26:23 +0000

    cool

--- app/shaders/1-vertices/fragment.glsl
+++ app/shaders/1-vertices/fragment.glsl
@@ -2,5 +2,10 @@ precision mediump float;
 uniform float t;
 
 void main() {
-  gl_FragColor = vec4(sin(t / 10.0), cos(t / 10.0), 0.5, 1);
+  // gl_FragCoord
+  float g = sin(gl_FragCoord.y / 10.0) * sin(gl_FragCoord.x / 10.0) + sin(t);
+  float r = sin(gl_FragCoord.x / 10.0) * sin(gl_FragCoord.y / 10.0) + sin(t);
+  float b = sin(gl_FragCoord.x / 500.0);
+  float a = sin(gl_FragCoord.x / 1000.0);
+  gl_FragColor = vec4(r, g, b, a);
 }
