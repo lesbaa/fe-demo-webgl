51df0d9807aa5d723ad000114afadc4c68d912be
Les Moffat <les@lesmoffat.co.uk>
2018-02-18 19:32:02 +0000

    update triangle

--- app/modules/les-gl/geometries/isosceles-triangle.js
+++ app/modules/les-gl/geometries/isosceles-triangle.js
@@ -1,17 +1,31 @@
+// with the texture map we're telling webgl how to map the texture to each vertex
+// where 0.0 
+
 export default function({
   w = 1.0,
   h = 1.0,
+  d = 1.0,
   x = 0.0,
   y = 0.0,
   z = 0.0,
+  texture = null,
 }) {
   return function() {
     return {
       vertices: [
-         0.0,     h,    0.0,
-        -(w / 2), 0.0,  0.0,
-         (w / 2), 0.0,  0.0,
+         0.0,  h/2,  0.0,
+        -w/2, -h/2,  0.0,
+         w/2, -h/2,  0.0,
       ],
+      textureMap: [
+        1.0,  0.0,
+        1.0,  1.0,
+        0.0,  0.0,
+        0.0,  1.0,
+      ],
+      tCols: 2,
+      tRows: 4,
+      texture,
       cols: 3,
       rows: 3,
       position: {
@@ -19,7 +33,7 @@ export default function({
         y,
         z,
       },
-      glPrimitive: this.gl.TRIANGLES,
+      glPrimitive: this.gl.TRIANGLE,
       rotation: {
         x: 0,
         y: 0,
@@ -28,3 +42,4 @@ export default function({
     }
   }
 }
+
