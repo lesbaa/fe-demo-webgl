0a9ed6f2a75eedca17b7db0461393c749e6af48a
Les Moffat <les.moffat@stv.tv>
2018-02-19 13:51:39 +0000

    basic scene

--- app/app.js
+++ app/app.js
@@ -1,93 +1,107 @@
+import * as THREE from 'three'
 import './modules/three-add-ons'
-import {
-  // SETUP
+import dotScreenShader from './shaders/dot-screen'
+import pixelateShader from './shaders/pixelate'
+import rgbShiftShader from './shaders/rgb-shift'
+import tvScreenShader from './shaders/tv-screen'
+
+const {
   Scene,
-  WebGLRenderer,
-  Mesh,
   PerspectiveCamera,
-
-  // GEOMETRIES
-  SphereBufferGeometry,
+  WebGLRenderer,
   BoxGeometry,
-  // LIGHTS
+  Mesh,
   AmbientLight,
-
-  // MATERIALS
-  MeshLambertMaterial,
-  MeshNormalMaterial,
-
-  // COMPOSER / SHADERS
-  EffectComposer,
-  // MATHS
-  Vector3,
-
-  // UTILS
+  AdditiveBlending,
   GridHelper,
-  Clock,
-
-} from 'three'
-import WebGLDebugUtils from 'webgl-debug'
+  SpotLight,
+  SphereGeometry,
+  TextureLoader,
+  SphericalReflectionMapping,
+  DoubleSide,
+  MeshNormalMaterial,
+  MeshLambertMaterial,
+  PlaneGeometry,
+  Vector3,
+  EffectComposer,
+  OrbitControls,
+  ShaderPass,
+  RenderPass,
+  PointLight,
+} = THREE
 
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl_postprocessing.html
 
-const aspect = window.innerWidth / window.innerHeight
-
 const canvas = document.getElementById('c')
 const scene = new Scene()
-const camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
-camera.position.y = 10
-camera.position.x = 20
-camera.position.z = 20
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const textureLoader = new TextureLoader()
+const RESOLUTION = 10
+const envMap = textureLoader.load('assets/texture.png')
+envMap.mapping = SphericalReflectionMapping
+
 const renderer = new WebGLRenderer({
   // preserveDrawingBuffer: true,
   canvas,
   antialias: true,
 })
 
-function throwOnGLError(err, funcName, args) {
-  throw WebGLDebugUtils.glEnumToString(err)
-  + "was caused by call to "
-  + funcName
-}
-
-renderer.context = WebGLDebugUtils.makeDebugContext(renderer.context, throwOnGLError)
-renderer.setPixelRatio( window.devicePixelRatio )
 renderer.setSize( canvas.offsetWidth, canvas.offsetHeight )
 
-const gridHelper = new GridHelper( 40, 10 )
-scene.add( gridHelper )
+const normalMaterial = new MeshLambertMaterial({
+  side: DoubleSide,
+  color: 0x66dddd,
+})
 
-const shape = new Mesh(
-  new BoxGeometry( 10, 10, 10 ),
-  new MeshNormalMaterial({
-    // side: DoubleSide,
-    color: 0xffffff,
-  })
-)
-
-scene.add(shape)
-
-const light = new AmbientLight(0xffffff, 40)
+const light = new PointLight( 0x44ddcc, 1.00)
+light.position.set(5, 5, 5)
 scene.add(light)
 
-const clock = new Clock()
+const lightTwo = new PointLight( 0xdd3333, 1.00)
+lightTwo.position.set(-5, -5, -5)
+scene.add(lightTwo)
 
-const loop = (time) => {
-  camera.lookAt(scene.position)  
-  shape.rotation.y += 0.01
-  shape.rotation.x += 0.01
-  shape.rotation.z += 0.01
-  renderer.render( scene, camera )
+const boxGeom = new BoxGeometry(1, 1, 1)
+const sphereGeom = new SphereGeometry(1, 50, 50)
+const groundGeom = new PlaneGeometry(1000, 1000, 1000, 10, 10)
+
+const cube = new Mesh(
+  boxGeom,
+  normalMaterial
+)
+
+
+const sphere = new Mesh(
+  sphereGeom,
+  normalMaterial
+)
+
+const ground = new Mesh(
+  groundGeom,
+  normalMaterial
+)
+
+scene.add(cube)
+scene.add(ground)
+
+ground.rotation.x = 1.5708
+ground.position.y = -10
+camera.position.z = 2.8
+camera.position.y = 1.7
+
+const loop = () => {
   requestAnimationFrame(loop)
+  
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  cube.rotation.z += 0.01
+  
+  camera.lookAt(new Vector3(
+    cube.position.x,
+    cube.position.y,
+    cube.position.z
+  ))
+  renderer.render( scene, camera )
 }
 
 loop()
-
-const resize = () => {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.updateProjectionMatrix()
-}
-
-window.addEventListener('resize', resize)
