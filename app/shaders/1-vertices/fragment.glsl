e1fef5e6310aa223771a3e03c22bdd4d293234bd
Les Moffat <les.moffat@stv.tv>
2018-03-12 13:19:44 +0000

    correct lines

--- app/shaders/1-vertices/fragment.glsl
+++ app/shaders/1-vertices/fragment.glsl
@@ -4,8 +4,8 @@ uniform float t;
 void main() {
   // gl_FragCoord, explain this
   // remember to change the phase for cool effects!
-  float g = sin(gl_FragCoord.y / 10.0) * sin(gl_FragCoord.x / 10.0) + sin(t);
   float r = sin(gl_FragCoord.x / 10.0) * sin(gl_FragCoord.y / 10.0) + sin(t);
+  float g = sin(gl_FragCoord.y / 10.0) * sin(gl_FragCoord.x / 10.0) + sin(t);
   float b = sin(gl_FragCoord.x / 500.0);
   float a = sin(gl_FragCoord.x / 1000.0);
   gl_FragColor = vec4(r, g, b, a);
